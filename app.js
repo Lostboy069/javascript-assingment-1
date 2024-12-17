d50aea268c2611ff39b576d86e1348c5ff8d26e5
Muhammad Salman <salmanumer2day@gmail.com>
2024-12-17 18:31:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+//Chapter No 1 Alerts
+
+//Question No 1
+//alert("Hello  welcome.");
+
+//Question No 2
+//alert("Error! Please enter correct password.");
+
+//Question No 3
+//alert("Welcome to JS Land \n Happy Coding");
+
+//Question No 4
+//alert("Welcome to JS Land");
+//alert("Happy Coding");
+
+//Question No 5
+//console.log("Hello..I can run JS through my web browsers console");
+
+
+
+//Chapter No 2 Variables for Strings
+
+//Question No 1
+
+//var myName= ("Muhammad Salman");
+//alert(MyName);
+
+//var message= ("Hello World!");
+//alert(message);
+
+//var bioData= ("Certified WebDeveloper");
+//alert(bioData);
+
+//Question No 2
+
+//var foodName=("Pizza\nPizz\nPiz\nPi\nP");
+//alert(foodName);
+
+//Question No 3
+
+//var email=("salmnop00@gmail.com");
+//alert(email);
+
+//Question No 4
+
+//var book=("A Quicker way to learn Java Script");
+//alert(book);
+
+//Question No 5
+
+//document.write("<h1>Thank you for choosing us as your next career move.We're very proud to be welcoming you to our company.</h1>")
+
+//Chapter No 3 Variables for Numbers
+
+//Question No 1
+
+//var age=(17);
+//alert(age);
+
+//Question No 2
+
+//var visitors=("You have visited this site 69 times.");
+//alert(visitors);
+
+//Question No 3
+
+//var birthYear="2008"
+//document.write("<h4>" + birthYear + "</h4>");
+
+//Question No 4
+
+//var customerName = (" Mr Monis ordered \n");
+//var quantity = (2);
+//var product = ("Mind(s) on \n");
+//var store = ("Lost Brand")
+
+//document.write(customerName + quantity + product + store)
